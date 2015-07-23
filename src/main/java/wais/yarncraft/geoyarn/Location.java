d17c7973751afc8697e0d6ef6244b702bc3746bc
Tom Blount <tb12g09@ecs.soton.ac.uk>
2015-07-23 16:35:21 +0100

    Boop

--- src/main/java/wais/yarncraft/geoyarn/Location.java
+++ src/main/java/wais/yarncraft/geoyarn/Location.java
@@ -7,20 +7,14 @@ import net.minecraft.client.entity.EntityPlayerSP;
 
 public abstract class Location {
 	public abstract boolean match(EntityPlayerSP player);
+	
+	
+	public static String[] PARAMETERS = {};
 
 	public static Location create(JSONObject jsonLocation){
-//		Location location = null;
-//		try {
-//			Class classDefinition = Class.forName(className);
-//			location = (Location) classDefinition.newInstance();
-//		} catch (InstantiationException e) {
-//			System.out.println(e);
-//		} catch (IllegalAccessException e) {
-//			System.out.println(e);
-//		} catch (ClassNotFoundException e) {
-//			System.out.println(e);
-//		}
-//		return location;
-		return new TagLocation("wet");
+		switch (jsonLocation.getString("type")){
+			case: return location;
+			default: throw new Exception();
+		}
 	}
 }
