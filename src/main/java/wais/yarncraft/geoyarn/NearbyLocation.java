dae3d39b9bbf7c9bc0e2ac21b8a9dc87b4b2ea31
Jonathan Scott <jonathan@jscott.me>
2015-07-23 16:47:21 +0100

    comm

--- src/main/java/wais/yarncraft/geoyarn/NearbyLocation.java
+++ src/main/java/wais/yarncraft/geoyarn/NearbyLocation.java
@@ -4,8 +4,12 @@ import net.minecraft.client.entity.EntityPlayerSP;
 
 public class NearbyLocation extends Location {
 	
+	private String nearby;
+	private int distance;
+	
 	public NearbyLocation(String nearby, int distance) {
-		
+		this.nearby = nearby;
+		this.distance = distance;
 	}
 	
 	@Override
