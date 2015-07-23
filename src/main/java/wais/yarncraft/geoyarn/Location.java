0a8160400e78d90044a1eb454c01ab7e66825eb8
Tom Blount <tb12g09@ecs.soton.ac.uk>
2015-07-23 16:24:32 +0100

    Location.create now creates single Location

--- src/main/java/wais/yarncraft/geoyarn/Location.java
+++ src/main/java/wais/yarncraft/geoyarn/Location.java
@@ -1,15 +1,26 @@
 package wais.yarncraft.geoyarn;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import net.minecraft.client.entity.EntityPlayerSP;
 
 public abstract class Location {
 	public abstract boolean match(EntityPlayerSP player);
 
-	public static Location[] create(JSONArray jsonArray) {
-		Location[] locs = new Location[1];
-		locs[0] = new TagLocation("wet");
-		return locs;
+	public static Location create(JSONObject jsonLocation){
+//		Location location = null;
+//		try {
+//			Class classDefinition = Class.forName(className);
+//			location = (Location) classDefinition.newInstance();
+//		} catch (InstantiationException e) {
+//			System.out.println(e);
+//		} catch (IllegalAccessException e) {
+//			System.out.println(e);
+//		} catch (ClassNotFoundException e) {
+//			System.out.println(e);
+//		}
+//		return location;
+		return new TagLocation("wet");
 	}
 }
