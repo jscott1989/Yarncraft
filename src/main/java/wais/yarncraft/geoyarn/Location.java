7291f968c5cbcb6674b3b003e1f211c438b1ab33
Tom Blount <tb12g09@ecs.soton.ac.uk>
2015-07-23 15:10:30 +0100

    About to test, need story.json

--- src/main/java/wais/yarncraft/geoyarn/Location.java
+++ src/main/java/wais/yarncraft/geoyarn/Location.java
@@ -1,7 +1,15 @@
 package wais.yarncraft.geoyarn;
 
+import org.json.JSONArray;
+
 import net.minecraft.client.entity.EntityPlayerSP;
 
 public abstract class Location {
 	public abstract boolean match(EntityPlayerSP player);
+
+	public static Location[] create(JSONArray jsonArray) {
+		Location[] locs = new Location[1];
+		locs[0] = new TagLocation("wet");
+		return locs;
+	}
 }
