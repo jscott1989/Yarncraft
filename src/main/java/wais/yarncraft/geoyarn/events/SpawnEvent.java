016e35f1ed912c841925a9a6b909a0e3c62fbb53
Tom Blount <tb12g09@ecs.soton.ac.uk>
2015-07-24 14:50:45 +0100

    Fixed spawny code

--- src/main/java/wais/yarncraft/geoyarn/events/SpawnEvent.java
+++ src/main/java/wais/yarncraft/geoyarn/events/SpawnEvent.java
@@ -1,8 +1,12 @@
 package wais.yarncraft.geoyarn.events;
 
+import java.util.Random;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class SpawnEvent extends GeoYarnEvent {
@@ -11,7 +15,7 @@ public class SpawnEvent extends GeoYarnEvent {
 	private String type;
 	private int number;
 	private EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-	
+	private Random rand = new Random();
 	
 	public SpawnEvent(String type, int number) {
 		this.type = type;
@@ -23,11 +27,16 @@ public class SpawnEvent extends GeoYarnEvent {
 		
 		for(int i=0; i<number; i++){
 			if (type.equals("villager")){
-				 entity = new EntityChicken(Minecraft.getMinecraft().theWorld);
+				 entity = new EntityVillager(Minecraft.getMinecraft().theWorld);
 			}
-			entity.setPositionAndUpdate(player.posX, player.posY, player.posZ);
+			entity.setPositionAndUpdate(player.posX + randomOffset(), player.posY, player.posZ+ randomOffset());
 			Minecraft.getMinecraft().theWorld.spawnEntityInWorld(entity);
+			((EntityLiving) entity).setAIMoveSpeed((float)0.5);
 		}
+	}
+	
+	public int randomOffset(){
+		return rand.nextInt(11) - 5;
 	}
 
 }
