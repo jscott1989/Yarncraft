2e6a134ada0422b688aef6a15ff5200ae4fccc0b
Jonathan Scott <jonathan@jscott.me>
2015-07-24 14:16:54 +0100

    full story - kind of

--- src/main/java/wais/yarncraft/geoyarn/NearbyLocation.java
+++ src/main/java/wais/yarncraft/geoyarn/NearbyLocation.java
@@ -1,6 +1,10 @@
 package wais.yarncraft.geoyarn;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 import wais.yarncraft.YarnCraft;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.item.Item;
@@ -18,16 +22,53 @@ public class NearbyLocation extends Location {
 	
 	@Override
 	public boolean match(EntityPlayerSP player) {
-		// Look in the area surrounding the player for the object specified by "nearby"
-//		for (int x = (int)player.lastTickPosX - distance; x < (int)player.lastTickPosX + distance; x++) {
-//			for (int y = (int)player.lastTickPosY - distance; y < (int)player.lastTickPosY + distance; y++) {
-//				for (int z = (int)player.lastTickPosZ - distance; z < (int)player.lastTickPosZ + distance; z++) {
-////					Item i = YarnCraft.getBlock(x, y, z).getItem(Minecraft.getMinecraft().theWorld, new BlockPos(x, y, z));
-////					System.out.println(x);
-//				}
-//			}
-//		}
-//		
+		// Check if there is a material with the same name as "Nearby"
+		for (Field field : Material.class.getDeclaredFields()) {
+		    if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+		        if (field.getName().equalsIgnoreCase(nearby)) {
+		        	// We're looking for a material
+		        	HashMap<Material, Integer> m = YarnCraft.countAroundPlayer(player, distance);
+		        	try {
+						return m.getOrDefault(field.get(null), 0) > 0;
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        }
+		    }
+		}
+		
+		// Next check for items
+		for (int x = (int)player.lastTickPosX - distance; x <= (int)player.lastTickPosX + distance; x++) {
+			for (int y = (int)player.lastTickPosY - distance; y <= (int)player.lastTickPosY + distance; y++) {
+				for (int z = (int)player.lastTickPosZ - distance; z <= (int)player.lastTickPosZ + distance; z++) {
+					Item i = YarnCraft.getBlock(x, y, z).getItem(Minecraft.getMinecraft().theWorld, new BlockPos(x, y, z));
+					if (i != null) {
+						if (i.getUnlocalizedName().equals(nearby)) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		
+		distance = 2;
+		//
+		for (int x = (int)player.lastTickPosX - distance; x <= (int)player.lastTickPosX + distance; x++) {
+			for (int y = (int)player.lastTickPosY - distance; y <= (int)player.lastTickPosY + distance; y++) {
+				for (int z = (int)player.lastTickPosZ - distance; z <= (int)player.lastTickPosZ + distance; z++) {
+					Item i = YarnCraft.getBlock(x, y, z).getItem(Minecraft.getMinecraft().theWorld, new BlockPos(x, y, z));
+					if (i != null && i.getUnlocalizedName().equals("tile.enchanmentTable")) {
+						System.out.println(i.getUnlocalizedName());
+					}
+				}
+			}
+		}
+		
+		
 		return false;
 	}
 
